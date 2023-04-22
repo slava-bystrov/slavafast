49bf64995bea5797de000afccf41b320f94d9fef
slava-bystrov <slava.bystrov.v@gmail.com>
2023-04-23 00:26:45 +0200

    Added sass + router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+import Home from './pages/home/Home';
+import './App.sass';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return <>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+      </ul>
+    </nav>
+
+    <Routes>
+      <Route
+        path="/"
+        element={<Home/>}
+      />
+    </Routes>
+  </>;
 }
-
-export default App;
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+      <div className="Home">
+        123d
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia aut laborum voluptates in fuga sint corrupti impedit? Repudiandae at vel molestias fugit exercitationem dolorum, architecto, delectus, enim sint dolor ea.</p>
+      </div>
+    );
+  }
+  
+  export default Home;
