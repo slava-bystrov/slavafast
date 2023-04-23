a325b5a6a0aff04ff7932d72f924aa5273a71dd6
slava-bystrov <slava.bystrov.v@gmail.com>
2023-04-23 17:04:54 +0200

    Routes config small update

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,18 @@ import {
 } from 'react-router-dom'
 import { AppRoutesConfig } from './constants/routes'
 import Navigation from './ui-kit/navigation/Navigation'
-import Home from './pages/home/Home'
-import Works from './pages/works/Works'
-import Blog from './pages/blog/Blog'
-import About from './pages/about/About'
+
 
 export default function App() {
   return <>
     <Navigation />
 
     <Routes>
-      <Route
-        path={AppRoutesConfig.Home.path}
-        element={<Home/>}
-      />
-      <Route
-        path={AppRoutesConfig.Works.path}
-        element={<Works/>}
-      />
-      <Route
-        path={AppRoutesConfig.Blog.path}
-        element={<Blog/>}
-      />
-      <Route
-        path={AppRoutesConfig.About.path}
-        element={<About/>}
-      />
+      {Object.values(AppRoutesConfig).map(routeConfig => <Route
+        key={routeConfig.path}
+        path={routeConfig.path}
+        element={routeConfig.element}
+      />)}
     </Routes>
   </>
 }
--- src/constants/routes.tsx
+++ src/constants/routes.tsx
@@ -1,18 +1,27 @@
+import Home from '../pages/home/Home'
+import Works from '../pages/works/Works'
+import Blog from '../pages/blog/Blog'
+import About from '../pages/about/About'
+
 export const AppRoutesConfig = {
   Home: {
     path: '/',
     title: 'Home',
+    element: <Home />
   },
   Works: {
     path: '/works',
     title: 'Works',
+    element: <Works />
   },
   Blog: {
     path: '/blog',
     title: 'Blog',
+    element: <Blog />
   },
   About: {
     path: '/about',
     title: 'About & Contacts',
+    element: <About />
   },
 }
