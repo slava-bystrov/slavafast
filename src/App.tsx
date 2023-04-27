c644f9085899c10bde3d1c619a57a888230c38b7
slava-bystrov <slava.bystrov.v@gmail.com>
2023-04-28 00:08:06 +0200

    Scrool to top on naviagation

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
+import { useEffect } from "react"
 import {
   Routes,
   Route,
+  useLocation
 } from 'react-router-dom'
 import { AppRoutesConfig } from './constants/routes'
 import Navigation from './ui-kit/navigation/Navigation'
 
+function ScrollToTop({ children }: any) {
+  const { pathname } = useLocation();
+  
+  useEffect(() => {
+      window.scrollTo(0, 0);
+  }, [pathname]);
+  
+  return children;
+}
+
 export default function App() {
   return <>
-    <Navigation />
+    <ScrollToTop>
+      <Navigation />
 
-    <Routes>
-      {Object.values(AppRoutesConfig).map(routeConfig => <Route
-        key={routeConfig.path}
-        path={routeConfig.path}
-        element={routeConfig.element}
-      />)}
-    </Routes>
+      <Routes>
+        {Object.values(AppRoutesConfig).map(routeConfig => <Route
+          key={routeConfig.path}
+          path={routeConfig.path}
+          element={routeConfig.element}
+        />)}
+      </Routes>
+    </ScrollToTop>
   </>
 }
